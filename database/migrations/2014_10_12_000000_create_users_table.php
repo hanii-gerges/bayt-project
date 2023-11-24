990edea15a25a231693dc84388c54ead38da497d
Hani Gerges <hanigerges05@gmail.com>
2023-11-24 17:05:09 +0300

    Add columns to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->tinyInteger('age');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('location');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
